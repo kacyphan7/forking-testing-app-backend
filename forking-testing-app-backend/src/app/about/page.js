fb82369b5adc83378a0c547bfcee2a44ed9730d0
Kacy Phan <kacyphann@gmail.com>
2023-07-08 14:17:16 -0700

    feat: add about component

---
+++ forking-testing-app-backend/src/app/about/page.js
@@ -0,0 +1,12 @@
+'use client';
+import { useEffect, useState } from 'react';
+
+export default function AboutFeature() {
+
+    return (
+        <main>
+            <h1>About Us:</h1>
+            <p>News Corp is a premier, global news agency dedicated to delivering unbiased and comprehensive news coverage. With a commitment to integrity and professionalism, we bring the world to your fingertips through timely, accurate reporting. Our passionate team of journalists works around the clock, ensuring you stay informed about the events that shape our world.</p>
+        </main>
+    );
+}
